35aad0dc0cc28b6772fe11d3894a6c2d7af0f668
oduffyibm <136842943+oduffyibm@users.noreply.github.com>
2024-12-03 17:27:36 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,11 +4,9 @@ import streamlit as st
 #def main():
 st.title("Gen-Z Calculator")
 
-n1 = st.text_input('', 'Enter first number here')
-n1 = int(n1)
+n1 = st.number_input('First number here')
 
-n2 = st.text_input('Second number')
-n2 = int(n2)
+n2 = st.text_input('Second number here')
 
 op = st.selectbox("Operator", ['multiply','add','subtract','divide'],on_change=calculate(n1,n2,op))
 
