3c7f32f49c52ac5b829f2fdfe10ebdfee9848529
oduffyibm <136842943+oduffyibm@users.noreply.github.com>
2024-12-03 17:28:43 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,26 @@
 
 import streamlit as st
 
-#def main():
-st.title("Gen-Z Calculator")
+def main():
+  st.title("Gen-Z Calculator")
+  
+  n1 = st.number_input('First number here')
+  
+  n2 = st.text_input('Second number here')
+  
+  op = st.selectbox("Operator", ['multiply','add','subtract','divide'],on_change=calculate(n1,n2,op))
 
-n1 = st.number_input('First number here')
 
-n2 = st.text_input('Second number here')
+def calculate(n1,n2,op):
+    if op is 'multiply':
+        ans = n1 * n2
+    elif op is 'divide':
+        ans = n1/n2
+    elif op is 'add':
+        ans = n1+n2
+    else:
+        ans = n1-n2
 
-op = st.selectbox("Operator", ['multiply','add','subtract','divide'],on_change=calculate(n1,n2,op))
+    st.write('Your answer is: ', ans)
 
-
-#def calculate(n1,n2,op):
-#    if op is 'multiply':
-#        ans = n1 * n2
-#    elif op is 'divide':
-##        ans = n1/n2
-#    elif op is 'add':
-#        ans = n1+n2
-#    else:
-#        ans = n1-n2
-
-#    st.write('Your answer is: ', ans)
-
-#main()
+main()
