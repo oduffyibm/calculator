2d757b5f8b70555d31ba71f9e57fb9a9b2430cb6
Owen Duffy <oduffy1@gmail.com>
2024-12-03 16:54:03 -0600

    calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Gen-Z Calculator")
+
+n1 = st.text_input('', value: str = 'Enter fisrt number here')
+
+n2 = st.text_input('Second number')
+
+op = st.selectbox("Operator", ['multiply','add','subtract','divide'])
+
+if op is 'multiply':
+    ans = n1 * n2
+elif op is 'divide':
+    ans = n1/n2
+elif op is 'add':
+    ans = n1+n2
+else:
+    ans = n1-n2
+
+st.write('Your answer is: ', ans)
+
