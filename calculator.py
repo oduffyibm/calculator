f10ebcf102af2608b7712eec0008d54059bc8523
Owen Duffy <oduffy1@gmail.com>
2024-12-03 17:09:51 -0600

    hahaha

--- calculator.py
+++ calculator.py
@@ -3,8 +3,10 @@ import streamlit as st
 st.title("Gen-Z Calculator")
 
 n1 = st.text_input('', 'Enter first number here')
+n1 = float(n1)
 
 n2 = st.text_input('Second number')
+n2 = float(n2)
 
 op = st.selectbox("Operator", ['multiply','add','subtract','divide'])
 
