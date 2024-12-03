f0bea19e9f56aa180f7e640bccbe70b39cef8473
Owen Duffy <oduffy1@gmail.com>
2024-12-03 17:16:35 -0600

    better

--- calculator.py
+++ calculator.py
@@ -1,23 +1,28 @@
 import streamlit as st
 
-st.title("Gen-Z Calculator")
+def main():
+    st.title("Gen-Z Calculator")
 
-n1 = st.text_input('', 'Enter first number here')
-n1 = float(n1)
+    n1 = st.text_input('', 'Enter first number here')
+    n1 = float(n1)
 
-n2 = st.text_input('Second number')
-n2 = float(n2)
+    n2 = st.text_input('Second number')
+    n2 = float(n2)
 
-op = st.selectbox("Operator", ['multiply','add','subtract','divide'])
+    op = st.selectbox("Operator", ['multiply','add','subtract','divide'],on_change=calculate())
 
-if op is 'multiply':
-    ans = n1 * n2
-elif op is 'divide':
-    ans = n1/n2
-elif op is 'add':
-    ans = n1+n2
-else:
-    ans = n1-n2
 
-st.write('Your answer is: ', ans)
+def calculate():
+    if op is 'multiply':
+        ans = n1 * n2
+    elif op is 'divide':
+        ans = n1/n2
+    elif op is 'add':
+        ans = n1+n2
+    else:
+        ans = n1-n2
 
+    st.write('Your answer is: ', ans)
+
+if __name__ == __main__:
+    main()
